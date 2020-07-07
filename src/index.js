3fe4bad281b8e3e43eeb73f302ac96eef56c6e2d
Enenlingre <enenlingre@gmail.com>
2020-07-07 21:49:29 +0300

    add sprite hero

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import * as PIXI from 'pixi.js';
+import hero from './hero.png';
 
+
+console.log(1);
 // The application will create a renderer using WebGL, if possible,
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container.
@@ -13,27 +16,38 @@ app.renderer.resize(window.innerWidth, window.innerHeight);
 // The application will create a canvas element for you that you
 // can then insert into the DOM.
 document.body.appendChild(app.view);
+
+const loader = new PIXI.Loader();
+
+loader.add(hero)
+  .load(setup);
+
+function setup() {
+  let sprite = new PIXI.Sprite(
+    loader.resources[hero].texture
+  );
+}
  
-// load the texture we need
-PIXI.loader.add('bunny', 'bunny.png').load((loader, resources) => {
+// // load the texture we need
+// PIXI.loader.add('ss','/Users/develop/Documents/save-yourself-game/dist/hero.png').load((loader, resources) => {
  
-    // This creates a texture from a 'bunny.png' image.
-    const bunny = new PIXI.Sprite(resources.bunny.texture);
+//     // This creates a texture from a 'bunny.png' image.
+//     const bunny = new PIXI.Sprite(resources['/Users/develop/Documents/save-yourself-game/dist/hero.png'].texture);
  
-    // Setup the position of the bunny
-    bunny.x = app.renderer.width / 2;
-    bunny.y = app.renderer.height / 2;
+//     // Setup the position of the bunny
+//     bunny.x = app.renderer.width / 2;
+//     bunny.y = app.renderer.height / 2;
  
-    // Rotate around the center
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
+//     // Rotate around the center
+//     bunny.anchor.x = 0.5;
+//     bunny.anchor.y = 0.5;
  
-    // Add the bunny to the scene we are building.
-    app.stage.addChild(bunny);
+//     // Add the bunny to the scene we are building.
+//     app.stage.addChild(bunny);
  
-    // Listen for frame updates
-    app.ticker.add(() => {
-         // each frame we spin the bunny around a bit
-        bunny.rotation += 0.01;
-    });
-});
+//     // Listen for frame updates
+//     app.ticker.add(() => {
+//          // each frame we spin the bunny around a bit
+//         bunny.rotation += 0.01;
+//     });
+// });
