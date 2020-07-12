99009c4cdb9563a47e8cd45c473d3aab1f69286e
Enenlingre <enenlingre@gmail.com>
2020-07-13 01:22:20 +0300

    add animated sprite

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { Application, Loader, Sprite } from 'pixi.js';
+import { Application, Loader, Sprite, Container, AnimatedSprite, Texture, Rectangle, BaseTexture } from 'pixi.js';
 import { createKeybord } from './createKeybord';
-import hero from './hero.png';
+import hero from './sprites/hero.png';
+import background from './sprites/background.jpg';
 
 
 console.log(1);
@@ -20,21 +21,32 @@ document.body.appendChild(app.view);
 
 const loader = new Loader();
 
-loader.add(hero)
+loader.add(background)
   .load(setup);
 
-let sprite;
+let sprite, gameScene, gameOverScene;
 
 let state;
 
 function setup() {
-  sprite = new Sprite(
-    loader.resources[hero].texture
-  );
+  gameScene = new Container();
+  app.stage.addChild(gameScene);
+  
+  gameOverScene = new Container();
+  app.stage.addChild(gameOverScene);
+
+  const er = new Sprite(loader.resources[background].texture);
+  gameScene.addChild(er); 
+
+  // const animatedSprite = new AnimatedSprite(json.animations["ahero"]);
+
+  sprite = getAnimatedSprite(hero);
+
   sprite.vx = 0;
   sprite.vy = 0;
 
-  app.stage.addChild(sprite);
+  gameScene.addChild(sprite);
+  sprite.play();
 
   const keyboard = createKeybord()
     .addKey('KeyW', () => {
@@ -49,6 +61,7 @@ function setup() {
       sprite.vy = 0;
     })
     .addKey('KeyA', () => {
+      console.log(sprite);
       sprite.vx = -1;
       console.log('erer');
     }, () => {
@@ -77,6 +90,74 @@ function play() {
   //position to make it move
   sprite.x += sprite.vx;
   sprite.y += sprite.vy;
+}
+
+function hitTestRectangle(r1, r2) {
+
+  //Define the variables we'll need to calculate
+  let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+  //hit will determine whether there's a collision
+  hit = false;
+
+  //Find the center points of each sprite
+  r1.centerX = r1.x + r1.width / 2;
+  r1.centerY = r1.y + r1.height / 2;
+  r2.centerX = r2.x + r2.width / 2;
+  r2.centerY = r2.y + r2.height / 2;
+
+  //Find the half-widths and half-heights of each sprite
+  r1.halfWidth = r1.width / 2;
+  r1.halfHeight = r1.height / 2;
+  r2.halfWidth = r2.width / 2;
+  r2.halfHeight = r2.height / 2;
+
+  //Calculate the distance vector between the sprites
+  vx = r1.centerX - r2.centerX;
+  vy = r1.centerY - r2.centerY;
+
+  //Figure out the combined half-widths and half-heights
+  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+  //Check for a collision on the x axis
+  if (Math.abs(vx) < combinedHalfWidths) {
+
+    //A collision might be occurring. Check for a collision on the y axis
+    if (Math.abs(vy) < combinedHalfHeights) {
+
+      //There's definitely a collision happening
+      hit = true;
+    } else {
+
+      //There's no collision on the y axis
+      hit = false;
+    }
+  } else {
+
+    //There's no collision on the x axis
+    hit = false;
+  }
+
+  //`hit` will be either `true` or `false`
+  return hit;
+};
+
+function getAnimatedSprite(src) {
+  const texture = BaseTexture.from(src);
+
+  const frames = Array.from(Array(3), (_, i) => {
+    const size = 50;
+    const x = size * i;
+
+    return  new Texture(texture, new Rectangle(x + 0, 0, size, size));
+    
+  });
+
+  const animatedSprite = new AnimatedSprite(frames);
+  animatedSprite.animationSpeed = 0.12
+
+  return animatedSprite;
 }
  
 // // load the texture we need
